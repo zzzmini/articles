ad0a98aa90a63be1259c08447e30a04d7b49c976
zzzmini <sm01079200887@gmail.com>
2024-10-14 16:03:34 +0900

    Articles CRUD를 위한 폼 작업 정리

--- src/main/java/com/my/articles/controller/ArticleController.java
+++ src/main/java/com/my/articles/controller/ArticleController.java
@@ -1,8 +1,45 @@
 package com.my.articles.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("articles")
 public class ArticleController {
+    @GetMapping("")
+    public String showAllArticles() {
+        return "/articles/show_all";
+    }
 
+    @GetMapping("new")
+    public String newArticle() {
+        return "/articles/new";
+    }
+
+    @PostMapping("create")
+    private String createArticle() {
+        return "redirect:articles";
+    }
+
+    @GetMapping("{id}")
+    public String showOneArticle() {
+        return "/articles/show";
+    }
+
+    @GetMapping("{id}/update")
+    public String viewUpdateArticle() {
+        return "/articles/update";
+    }
+
+    @PostMapping("update")
+    public String updateArticle() {
+        return "redirect:articles";
+    }
+
+    @GetMapping("{id}/delete")
+    public String deleteArticle() {
+        return "redirect:articles";
+    }
 }
