07a4cba68c915ea83e587d9dd90541d670b9e0e4
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-10-16 15:12:29 +0900

    양방향연관관계처리종료 (#9)

---
+++ src/main/java/com/my/articles/dto/CommentDTO.java
@@ -0,0 +1,29 @@
+package com.my.articles.dto;
+
+import com.my.articles.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CommentDTO {
+    private Long id;
+    private String nickname;
+    private String body;
+
+    public static CommentDTO fromEntity(Comment comment) {
+        return new CommentDTO(
+                comment.getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
+
+    public static Comment fromDto(CommentDTO dto) {
+        Comment comment = new Comment();
+        comment.setId(dto.getId());
+        comment.setNickname(dto.getNickname());
+        comment.setBody(dto.getBody());
+        return comment;
+    }
+}
---
+++ src/main/java/com/my/articles/entity/Comment.java
@@ -0,0 +1,21 @@
+package com.my.articles.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nickname;
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
